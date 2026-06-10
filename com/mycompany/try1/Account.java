50cb6ad80f65cac9059714037974f0d5c4baeaab
DRGSpiritZ1 <farouqiskandr911@gmail.com>
2026-06-10 10:45:05 +0800

    Update Account.java

--- com/mycompany/try1/Account.java
+++ com/mycompany/try1/Account.java
@@ -1,9 +1,11 @@
 package com.mycompany.try1;
 
+// Model class
 public class Account {
     private String username;
     private String password;
 
+    // sets  details when user account is created
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
@@ -13,10 +15,12 @@ public class Account {
         return username;
     }
 
+    // checks user entered password mathces the stored password
     public boolean checkPassword(String enteredPassword) {
         return password.equals(enteredPassword);
     }
 
+    // used to retirve password to save in file
     public String getStoredPassword() {
         return password;
     }
